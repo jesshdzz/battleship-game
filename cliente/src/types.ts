dd659aec90ed4a2912995455db87acb050507f7c
jesshdzz <jesushernandezz.dev@gmail.com>
2025-12-03 21:56:26 -0600

    ADD: Basic Game Types

---
+++ cliente/src/types.ts
@@ -0,0 +1,54 @@
+// --- TIPOS BÁSICOS ---
+
+export type PlayerId = string; // El Socket ID
+export type RoomId = string;   // Código de la sala (ej: "XJ9-12")
+
+export type Coordinate = {
+    x: number;
+    y: number;
+};
+
+// --- BARCOS ---
+export type ShipType = 'carrier' | 'battleship' | 'cruiser' | 'submarine' | 'destroyer';
+
+export interface Ship {
+    id: string;
+    type: ShipType;
+    size: number;
+    positions: Coordinate[]; // Dónde está colocado
+    hits: number;            // Cuántas veces le han dado
+    sunk: boolean;           // ¿Hundido?
+}
+
+// --- TABLERO ---
+// El estado de una celda para mostrar en pantalla
+export type CellState = 'EMPTY' | 'SHIP' | 'HIT' | 'MISS';
+
+// --- ESTADO DEL JUEGO (Lo que vive en el servidor) ---
+export interface Player {
+    id: PlayerId;
+    name: string;
+    ready: boolean;      // ¿Ya puso sus barcos?
+    ships: Ship[];
+    myBoard: CellState[][];    // Tablero propio
+    shotsFired: Coordinate[];  // Historial de disparos hechos
+}
+
+export interface GameState {
+    roomId: RoomId;
+    players: Player[];   // Máximo 2 jugadores
+    status: 'LOBBY' | 'PLACING_SHIPS' | 'PLAYING' | 'GAME_OVER';
+    turn: PlayerId;      // ID del jugador que le toca disparar
+    winner: PlayerId | null;
+}
+
+// --- EVENTOS DE SOCKET (La comunicación) ---
+// Estos son los nombres de los mensajes que enviaremos
+export const EVENTS = {
+    JOIN_ROOM: 'join_room',
+    CREATE_ROOM: 'create_room',
+    GAME_UPDATE: 'game_update', // El servidor envía el estado nuevo
+    PLACE_SHIP: 'place_ship',
+    FIRE_SHOT: 'fire_shot',
+    ERROR: 'error_msg'
+};
